258196e5883cedc82c3c8a37146381fbed9c756f
Abdullah <abdullah.akcam52@gmail.com>
2020-06-22 11:59:50 +0300

    proxy

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ app.use(session({
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.enable('trust proxy')
 // Passport
 app.use(passport.initialize());
 app.use(passport.session());
