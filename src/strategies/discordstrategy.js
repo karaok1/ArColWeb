83be8ab4e85108ec750dfa03a766a86e93eb4796
Abdullah <abdullah.akcam52@gmail.com>
2020-06-23 22:46:24 +0300

    hotfix

--- src/strategies/discordstrategy.js
+++ src/strategies/discordstrategy.js
@@ -18,7 +18,7 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new DiscordStrategy({
     clientID: '691377322116251688',
     clientSecret: 'n_-ZuuOj8Uj0bfnvnlJseQZEqqKrgNBY',
-    callbackURL: '/auth/redirect',
+    callbackURL: 'https://akchan.me/auth/redirect',
     scope: ['identify', 'guilds']
 }, async (accessToken, refreshToken, profile, done) => {
     try {
